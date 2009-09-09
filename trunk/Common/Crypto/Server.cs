6effe4804494c519a8cb47a55608361e2f784d15
exception <exception@c8311042-8cf6-48b5-8338-4c684ad76273>
2009-09-09 14:52:44 +0000

    Crypto Structure
    git-svn-id: http://projects.piratenpartei.ch:8080/pi-vote@13 c8311042-8cf6-48b5-8338-4c684ad76273

---
+++ trunk/Common/Crypto/Server.cs
@@ -0,0 +1,18 @@
+﻿/*
+ *  <project description>
+ * 
+ *  Copyright (c) 2008-2009 Stefan Thöni <stefan@savvy.ch> 
+ *  <BSD Like license>
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pirate.PiVote.Crypto
+{
+  public class Server
+  {
+  }
+}
