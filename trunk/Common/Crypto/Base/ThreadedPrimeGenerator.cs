23987878dd4a596ee436d3396b1e27ae5ebd71a9
exception <exception@c8311042-8cf6-48b5-8338-4c684ad76273>
2010-05-04 11:22:51 +0000

    Safe prime number generation for 4096 bit down to 13 min avg and 25 min worst case.
    git-svn-id: http://projects.piratenpartei.ch:8080/pi-vote@95 c8311042-8cf6-48b5-8338-4c684ad76273

--- trunk/Common/Crypto/Base/ThreadedPrimeGenerator.cs
+++ trunk/Common/Crypto/Base/ThreadedPrimeGenerator.cs
@@ -46,56 +46,28 @@ namespace Pirate.PiVote.Crypto
     /// </summary>
     private BigInt safePrime;
 
-    private int tested;
-    private int safePrimes;
-
-    private void PrimeTest()
+    /// <summary>
+    /// Runs an infinite find test to determine time to result.
+    /// </summary>
+    public void FindTest()
     {
       Console.WriteLine();
       Console.WriteLine();
-
-      this.tested = 0;
-      this.safePrimes = 0;
-      DateTime start = DateTime.Now;
-      DateTime last = DateTime.Now;
-
-      Environment.ProcessorCount.Times(() => new Thread(PrimeTestThread).Start());
+      Console.WriteLine();
+      TimeSpan time0 = new TimeSpan(0);
+      int counter = 0;
 
       while (true)
       {
-        if (DateTime.Now.Subtract(last).TotalSeconds > 5)
-        {
-          last = DateTime.Now;
-          string since = DateTime.Now.Subtract(start).ToString();
-          double safePrimePercent = 100d / (double)this.tested * (double)this.safePrimes;
-          double safePrimePerSecond = (double)this.safePrimes / DateTime.Now.Subtract(start).TotalSeconds;
-          Console.WriteLine("{0}: {1,10} / {2,10} = {3:0.00000}%, {4:0.00000}", since, this.safePrimes, this.tested, safePrimePercent, safePrimePerSecond);
-        }
-      }
-    }
-
-    private void PrimeTestThread()
-    {
-      BigInt number = Prime.RandomNumber(4096);
-      BigInt safeNumber;
-
-      while (true)
-      {
-        //number = Prime.RandomNumber(64).NextPrimeGMP();
-        number = number.NextPrimeGMP();
-        safeNumber = number * 2 + 1;
-        Interlocked.Increment(ref this.tested);
-
-        if (safeNumber.IsProbablyPrimeRabinMiller(1))
-        {
-          if (number.IsProbablyPrimeRabinMiller(64))
-          {
-            if (safeNumber.IsProbablyPrimeRabinMiller(64))
-            {
-              Interlocked.Increment(ref this.safePrimes);
-            }
-          }
-        }
+        DateTime start0 = DateTime.Now;
+        Emil.GMP.BigInt p0 = null;
+        Emil.GMP.BigInt sp0 = null;
+        ThreadedPrimeGenerator g0 = new ThreadedPrimeGenerator();
+        g0.FindPrimeAndSafePrime(4096, out p0, out sp0);
+        time0 = time0.Add(DateTime.Now.Subtract(start0));
+        counter++;
+        double seconds = time0.TotalSeconds / (double)counter;
+        Console.WriteLine("avg={0}", DateTime.Now.AddSeconds(seconds).Subtract(DateTime.Now).ToString());
       }
     }
 
@@ -126,39 +98,12 @@ namespace Pirate.PiVote.Crypto
       prime = this.prime;
       safePrime = this.safePrime;
 
-      Console.WriteLine("FindPrimeAndSafePrime    " + DateTime.Now.Subtract(start).ToString());
-    }
-    
-    /// <summary>
-    /// Finds both a prime and a larger safe prime.
-    /// </summary>
-    /// <param name="bitLength">Length in bits for the numbers.</param>
-    /// <param name="prime">Returns the prime.</param>
-    /// <param name="safePrime">Returns the safe prime.</param>
-    public void FindPrimeAndSafePrime2(int bitLength, out BigInt prime, out BigInt safePrime)
-    {
-      DateTime start = DateTime.Now;
-
-      this.bitLength = bitLength;
-
-      List<Thread> threads = new List<Thread>();
-      Environment.ProcessorCount.Times(() => threads.Add(new Thread(FindPrime)));
-      threads.ForEach(thread => thread.Start());
-      while (this.prime == null) { Thread.Sleep(100); }
-      threads.ForEach(thread => thread.Join());
-
-      threads = new List<Thread>();
-      Environment.ProcessorCount.Times(() => threads.Add(new Thread(FindSafePrime2)));
-      threads.ForEach(thread => thread.Start());
-      while (this.safePrime == null) { Thread.Sleep(100); }
-      threads.ForEach(thread => thread.Join());
-
-      prime = this.prime;
-      safePrime = this.safePrime;
-
       Console.WriteLine("FindPrimeAndSafePrime2   " + DateTime.Now.Subtract(start).ToString());
     }
 
+    /// <summary>
+    /// Large number of small primes for testing.
+    /// </summary>
     private static int[] SmallPrimes = new int[] { 
 2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
 31, 37, 41, 43, 47, 53, 59, 61, 67, 71,
@@ -1161,33 +1106,12 @@ namespace Pirate.PiVote.Crypto
 104549, 104551, 104561, 104579, 104593, 104597, 104623, 104639, 104651, 104659,
 104677, 104681, 104683, 104693, 104701, 104707, 104711, 104717, 104723, 104729 };
 
-////2,  3, 	5, 	7, 	11, 	13, 	17, 	19, 	23, 	29, 	31, 	37, 	41, 	43, 	47, 	53 ,	59, 	61, 	67, 	71,
-////73,	79,	83,	89,	97,	101,	103,	107,	109,	113,	127,	131,	137,	139,	149,	151,	157,	163,	167,	173,
-////179,	181,	191,	193,	197,	199,	211,	223,	227,	229,	233,	239,	241,	251,	257,	263,	269,	271,	277,	281,
-////283,	293,	307,	311,	313,	317,	331,	337,	347,	349,	353,	359,	367,	373,	379,	383,	389,	397,	401,	409,
-////419,	421,	431,	433,	439,	443,	449,	457,	461,	463,	467,	479,	487,	491,	499,	503,	509,	521,	523,	541,
-////547,	557,	563,	569,	571,	577,	587,	593,	599,	601,	607,	613,	617,	619,	631,	641,	643,	647,	653,	659,
-////661,	673,	677,	683,	691,	701,	709,	719,	727,	733,	739,	743,	751,	757,	761,	769,	773,	787,	797,	809,
-////811,	821,	823,	827,	829,	839,	853,	857,	859,	863,	877,	881,	883,	887,	907,	911,	919,	929,	937,	941,
-////947,	953,	967,	971,	977,	983,	991,	997,	1009,	1013,	1019,	1021,	1031,	1033,	1039,	1049,	1051,	1061,	1063,	1069,
-////1087,	1091,	1093,	1097,	1103,	1109,	1117,	1123,	1129,	1151,	1153,	1163,	1171,	1181,	1187,	1193,	1201,	1213,	1217,	1223,
-////1229,	1231,	1237,	1249,	1259,	1277,	1279,	1283,	1289,	1291,	1297,	1301,	1303,	1307,	1319,	1321,	1327,	1361,	1367,	1373,
-////1381,	1399,	1409,	1423,	1427,	1429,	1433,	1439,	1447,	1451,	1453,	1459,	1471,	1481,	1483,	1487,	1489,	1493,	1499,	1511,
-////1523,	1531,	1543,	1549,	1553,	1559,	1567,	1571,	1579,	1583,	1597,	1601,	1607,	1609,	1613,	1619,	1621,	1627,	1637,	1657,
-////1663,	1667,	1669,	1693,	1697,	1699,	1709,	1721,	1723,	1733,	1741,	1747,	1753,	1759,	1777,	1783,	1787,	1789,	1801,	1811,
-////1823,	1831,	1847,	1861,	1867,	1871,	1873,	1877,	1879,	1889,	1901,	1907,	1913,	1931,	1933,	1949,	1951,	1973,	1979,	1987,
-////1993,	1997,	1999,	2003,	2011,	2017,	2027,	2029,	2039,	2053,	2063,	2069,	2081,	2083,	2087,	2089,	2099,	2111,	2113,	2129,
-////2131,	2137,	2141,	2143,	2153,	2161,	2179,	2203,	2207,	2213,	2221,	2237,	2239,	2243,	2251,	2267,	2269,	2273,	2281,	2287,
-////2293,	2297,	2309,	2311,	2333,	2339,	2341,	2347,	2351,	2357,	2371,	2377,	2381,	2383,	2389,	2393,	2399,	2411,	2417,	2423,
-////2437,	2441,	2447,	2459,	2467,	2473,	2477,	2503,	2521,	2531,	2539,	2543,	2549,	2551,	2557,	2579,	2591,	2593,	2609,	2617,
-////2621,	2633,	2647,	2657,	2659,	2663,	2671,	2677,	2683,	2687,	2689,	2693,	2699,	2707,	2711,	2713,	2719,	2729,	2731,	2741,
-////2749,	2753,	2767,	2777,	2789,	2791,	2797,	2801,	2803,	2819,	2833,	2837,	2843,	2851,	2857,	2861,	2879,	2887,	2897,	2903,
-////2909,	2917,	2927,	2939,	2953,	2957,	2963,	2969,	2971,	2999,	3001,	3011,	3019,	3023,	3037,	3041,	3049,	3061,	3067,	3079,
-////3083,	3089,	3109,	3119,	3121,	3137,	3163,	3167,	3169,	3181,	3187,	3191,	3203,	3209,	3217,	3221,	3229,	3251,	3253,	3257,
-////3259,	3271,	3299,	3301,	3307,	3313,	3319,	3323,	3329,	3331,	3343,	3347,	3359,	3361,	3371,	3373,	3389,	3391,	3407,	3413,
-////3433,	3449,	3457,	3461,	3463,	3467,	3469,	3491,	3499,	3511,	3517,	3527,	3529,	3533,	3539,	3541,	3547,	3557,	3559,	3571 };
-
-    private bool SmallPrimesTest(BigInt number)
+    /// <summary>
+    /// Tests wether or not the number is divisible by any small prime.
+    /// </summary>
+    /// <param name="number">Number to test.</param>
+    /// <returns>Could it be prime?</returns>
+    private bool DivisiblyBySmallPrime(BigInt number)
     {
       foreach (int smallPrime in SmallPrimes)
       {
@@ -1198,7 +1122,13 @@ namespace Pirate.PiVote.Crypto
       return true;
     }
 
-    private bool SmallPrimesTest(BigInt number, BigInt doubleNumber)
+    /// <summary>
+    /// Tests wether or not any of the number is divisible by any small prime.
+    /// </summary>
+    /// <param name="number">Number to test.</param>
+    /// <param name="doubleNumber">Number to test.</param>
+    /// <returns>Could they both be prime?</returns>
+    private bool DivisiblyBySmallPrime(BigInt number, BigInt doubleNumber)
     {
       foreach (int smallPrime in SmallPrimes)
       {
@@ -1243,7 +1173,7 @@ namespace Pirate.PiVote.Crypto
     /// <summary>
     /// Thread work to find a safe prime.
     /// </summary>
-    private void FindSafePrime2()
+    private void FindSafePrime()
     {
       BigInt number;
       BigInt doubleNumber;
@@ -1254,7 +1184,7 @@ namespace Pirate.PiVote.Crypto
         if (number.IsEven) number++;
         doubleNumber = number * 2 + 1;
 
-        if (SmallPrimesTest(number, doubleNumber))
+        if (DivisiblyBySmallPrime(number, doubleNumber))
         {
           if (number.IsProbablyPrimeRabinMiller(LowRabinMillerCount))
           {
@@ -1276,7 +1206,8 @@ namespace Pirate.PiVote.Crypto
     /// <summary>
     /// Thread work to find a safe prime.
     /// </summary>
-    private void FindSafePrime()
+    [Obsolete]
+    private void FindSafePrimeOld()
     {
       BigInt number;
       BigInt doubleNumber;
@@ -1288,7 +1219,7 @@ namespace Pirate.PiVote.Crypto
         doubleNumber = number * 2 + 1;
         halfNumber = (number - 1) / 2;
 
-        if (this.safePrime == null && SmallPrimesTest(doubleNumber))
+        if (this.safePrime == null && DivisiblyBySmallPrime(doubleNumber))
         {
           if (this.safePrime == null && doubleNumber.IsProbablyPrimeRabinMiller(LowRabinMillerCount))
           {
@@ -1302,7 +1233,7 @@ namespace Pirate.PiVote.Crypto
           }
         }
 
-        if (this.safePrime == null && SmallPrimesTest(halfNumber))
+        if (this.safePrime == null && DivisiblyBySmallPrime(halfNumber))
         {
           if (this.safePrime == null && halfNumber.IsProbablyPrimeRabinMiller(LowRabinMillerCount))
           {
