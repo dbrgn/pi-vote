74f3da652c603515daa440ec4e21a57309edb7de
exception <exception@c8311042-8cf6-48b5-8338-4c684ad76273>
2010-05-16 12:15:11 +0000

    Forgot some files.
    git-svn-id: http://projects.piratenpartei.ch:8080/pi-vote@131 c8311042-8cf6-48b5-8338-4c684ad76273

---
+++ trunk/PiVoteUnitTest/Crypto/Base/BaseVotingTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Pirate.PiVote;
+using Pirate.PiVote.Crypto;
+using Emil.GMP;
+
+namespace PiVoteUnitTest
+{
+  [TestClass]
+  public class BaseVotingTest
+  {
+    private VotingParameters parameters;
+    private BigInt publicKey;
+
+    public BaseVotingTest()
+    {
+    }
+
+    [TestInitialize()]
+    public void MyTestInitialize()
+    {
+      this.parameters = VotingParameters.CreateTestParameters();
+    }
+
+    [TestCleanup()]
+    public void MyTestCleanup()
+    {
+    }
+
+    [TestMethod]
+    public void VotingTest()
+    {
+      List<Authority> auths = new List<Authority>();
+
+      for (int authIndex = 1; authIndex <= 5; authIndex++)
+      {
+        auths.Add(new Authority(authIndex, this.parameters));
+      }
+
+      auths.ForEach(a => a.CreatePolynomial());
+
+      var verVals = new List<List<VerificationValue>>();
+
+      for (int authIndex = 1; authIndex <= 5; authIndex++)
+      {
+        verVals.Add(new List<VerificationValue>(auths.Select(a => a.VerificationValue(authIndex))));
+      }
+
+      for (int authIndex = 1; authIndex <= 5; authIndex++)
+      {
+        Authority auth = auths[authIndex];
+        var shares = new List<Share>(auths.Select(a => a.Share(authIndex)));
+        Assert.IsTrue(auth.VerifySharing(shares, verVals));
+      }
+    }
+  }
+}
---
+++ trunk/PiVoteUnitTest/Crypto/Certificate/EncryptedTest.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Pirate.PiVote;
+using Pirate.PiVote.Crypto;
+
+namespace PiVoteUnitTest
+{
+  [TestClass]
+  public class EncryptedTest
+  {
+    private CertificateStorage storage;
+    private CACertificate root;
+    private CACertificate intermediate;
+    private AdminCertificate admin;
+    private AdminCertificate eve;
+
+    public EncryptedTest()
+    {
+    }
+
+    [TestInitialize()]
+    public void MyTestInitialize()
+    {
+      this.storage = new CertificateStorage();
+
+      this.root = new CACertificate("Root");
+      this.root.CreateSelfSignature();
+      this.storage.AddRoot(this.root.OnlyPublicPart);
+
+      var rootCrl = new RevocationList(this.root.Id, DateTime.Now, DateTime.Now.AddDays(1), new Guid[] { });
+      var signedRootCrl = new Signed<RevocationList>(rootCrl, this.root);
+      this.storage.AddRevocationList(signedRootCrl);
+
+      this.intermediate = new CACertificate("Intermediate");
+      this.intermediate.CreateSelfSignature();
+      this.intermediate.AddSignature(this.root, DateTime.Now.AddDays(1));
+      this.storage.Add(intermediate.OnlyPublicPart);
+
+      var intermediateCrl = new RevocationList(this.intermediate.Id, DateTime.Now, DateTime.Now.AddDays(1), new Guid[] { });
+      var signedIntermediateCrl = new Signed<RevocationList>(intermediateCrl, this.intermediate);
+      this.storage.AddRevocationList(signedIntermediateCrl);
+
+      this.admin = new AdminCertificate("Test");
+      this.admin.CreateSelfSignature();
+      this.admin.AddSignature(this.intermediate, DateTime.Now.AddDays(1));
+
+      this.eve = new AdminCertificate("Eve");
+      this.eve.CreateSelfSignature();
+      this.eve.AddSignature(this.intermediate, DateTime.Now.AddDays(1));
+    }
+
+    [TestCleanup()]
+    public void MyTestCleanup()
+    {
+    }
+
+    [TestMethod]
+    public void DecryptionTest()
+    {
+      Option option = new Option(new MultiLanguageString("Test"), new MultiLanguageString(string.Empty));
+
+      Encrypted<Option> encryptedOption = new Encrypted<Option>(option, this.admin.OnlyPublicPart);
+
+      Option other = encryptedOption.Decrypt(this.admin);
+
+      Assert.IsTrue(other.ToBinary().Equal(option.ToBinary()));
+    }
+
+    [TestMethod()]
+    [ExpectedException(typeof(ArgumentException))]
+    public void NonDecryptionTest()
+    {
+      Option option = new Option(new MultiLanguageString("Test"), new MultiLanguageString(string.Empty));
+
+      Encrypted<Option> encryptedOption = new Encrypted<Option>(option, this.admin.OnlyPublicPart);
+
+      Option other = encryptedOption.Decrypt(this.eve);
+    }
+  }
+}
